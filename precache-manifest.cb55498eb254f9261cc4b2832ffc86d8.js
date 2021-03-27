241f3cc8d10859cba8a0338f15749d618da172c7
Salachar <salachar@gmail.com>
2021-03-26 19:05:37 -0700

    Updates

---
+++ precache-manifest.cb55498eb254f9261cc4b2832ffc86d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60262addfe11b3ea9a86af52ac323d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0164273f0d66bdc735",
+    "url": "/static/css/main.bb0eb608.chunk.css"
+  },
+  {
+    "revision": "10640049e9acd072d63b",
+    "url": "/static/js/2.f7d82003.chunk.js"
+  },
+  {
+    "revision": "0a0164273f0d66bdc735",
+    "url": "/static/js/main.1f6fb288.chunk.js"
+  },
+  {
+    "revision": "72e304ddaf94cdc946ac",
+    "url": "/static/js/runtime-main.6d59f04d.js"
+  }
+]);
