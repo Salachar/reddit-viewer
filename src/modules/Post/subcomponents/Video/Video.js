b36ce44b804b76a0005bd272121d4121650e50cf
Salachar <salachar@gmail.com>
2021-04-01 20:52:22 -0700

    work on video player

--- src/modules/Post/subcomponents/Video/Video.js
+++ src/modules/Post/subcomponents/Video/Video.js
@@ -14,9 +14,19 @@ class Video extends Component {
         this.ref_video = React.createRef();
         this.ref_audio = React.createRef();
 
+        this.state = {
+            controls_ready: false,
+            paused: true,
+            muted: false,
+            duration: null,
+            current_time: null,
+            volume: 0.5,
+        };
+
         this.ready_to_play = {
             video: false,
             audio: false,
+            complete: false,
         };
 
         this.ref_embedded_media = React.createRef();
@@ -35,10 +45,21 @@ class Video extends Component {
     }
 
     onCanPlay (type) {
+        const {
+            volume,
+        } = this.state;
+
         this.ready_to_play[type] = true;
-        if (this.ready_to_play.video && this.ready_to_play.audio) {
-            // this.ref_video.current.play();
-            // this.ref_audio.current.play();
+        if (this.ready_to_play.video && this.ready_to_play.audio && !this.ready_to_play.complete) {
+            // We only want this to fire the first time the video and audio are ready
+            // and not after things like clicking on the progress bar
+            this.ready_to_play.complete = true;
+            this.ref_audio.current.volume = volume;
+            this.setState({
+                controls_ready: true,
+                duration: this.ref_video.current.duration,
+                current_time: 0,
+            });
         }
     }
 
@@ -46,6 +67,14 @@ class Video extends Component {
         const {
             post = {},
         } = this.props;
+        const {
+            controls_ready,
+            paused,
+            muted,
+            current_time,
+            duration,
+            volume,
+        } = this.state;
         const {
             secure_media = {},
             secure_media_embed = {}
@@ -64,35 +93,121 @@ class Video extends Component {
                         onCanPlay={() => {
                             this.onCanPlay('video');
                         }}
+                        onTimeUpdate={() => {
+                            this.setState({
+                                current_time: this.ref_video.current.currentTime,
+                            });
+                        }}
+                        onClick={() => {
+                            if (this.ref_video.current.paused) {
+                                this.ref_video.current.play();
+                                this.ref_audio.current.play();
+                                this.setState({
+                                    paused: false,
+                                });
+                            } else {
+                                this.ref_video.current.pause();
+                                this.ref_audio.current.pause();
+                                this.setState({
+                                    paused: true,
+                                });
+                            }
+                        }}
                     />
                     <audio
                         className={styles.audio}
                         ref={this.ref_audio}
                         src={audio}
+                        volume={volume}
                         onCanPlay={() => {
                             this.onCanPlay('audio');
                         }}
                     />
-                    <div className={styles.controls_wrapper}>
-                        <div
-                            className={classnames(styles.control, styles.playpause)}
-                            onClick={() => {
-                                if (this.ref_video.current.paused) {
-                                    this.ref_video.current.play();
-                                    this.ref_audio.current.play();
-                                } else {
-                                    this.ref_video.current.pause();
-                                    this.ref_audio.current.pause();
-                                }
-                            }}
-                        >PLAYPAUSE</div>
-                        <div
-                            className={classnames(styles.control, styles.mute)}
-                            onClick={() => {
-                                this.ref_audio.current.muted = !this.ref_audio.current.muted;
-                            }}
-                        >MUTE</div>
-                    </div>
+                    {controls_ready && (
+                        <div className={styles.controls}>
+                            <i
+                                className={classnames(styles.control, styles.playpause_icon,{
+                                    "fas fa-pause": !paused,
+                                    "fas fa-play": paused,
+                                })}
+                                onClick={() => {
+                                    if (this.ref_video.current.paused) {
+                                        this.ref_video.current.play();
+                                        this.ref_audio.current.play();
+                                        this.setState({
+                                            paused: false,
+                                        });
+                                    } else {
+                                        this.ref_video.current.pause();
+                                        this.ref_audio.current.pause();
+                                        this.setState({
+                                            paused: true,
+                                        });
+                                    }
+                                }}
+                            />
+                            <div className={classnames(styles.bar_wrapper, styles.progress_bar_wrapper)}>
+                                <div className={styles.bar} onClick={(e) =>{
+                                    const rect = e.currentTarget.getBoundingClientRect();
+                                    const { left } = rect;
+
+                                    const x_diff = e.pageX - left;
+                                    const per = x_diff / e.currentTarget.clientWidth;
+                                    const new_time = duration * per;
+
+                                    if (new_time < 0) new_time = 0;
+                                    if (new_time > this.duration) new_time = this.duration;
+
+                                    this.ref_video.current.currentTime = new_time;
+                                    this.ref_audio.current.currentTime = new_time;
+
+                                    this.setState({
+                                        current_time: new_time,
+                                    });
+                                }}>
+                                    <div className={styles.progress} style={{
+                                        width: `${(current_time / duration) * 100}%`,
+                                    }} />
+                                </div>
+                                <div className={styles.current_time}>{Math.round(current_time)}</div>
+                                <div className={styles.duration}>{Math.round(duration)}</div>
+                            </div>
+                            <i
+                                className={classnames(styles.control, styles.volume_icon, {
+                                    "fas fa-volume-off": !muted,
+                                    "fas fa-volume-mute": muted,
+                                })}
+                                onClick={() => {
+                                    this.ref_audio.current.muted = !this.ref_audio.current.muted;
+                                    this.setState({
+                                        muted: this.ref_audio.current.muted,
+                                    });
+                                }}
+                            />
+                            <div className={classnames(styles.bar_wrapper, styles.volume_bar_wrapper)}>
+                                <div className={styles.bar} onClick={(e) =>{
+                                    const rect = e.currentTarget.getBoundingClientRect();
+                                    const { left } = rect;
+
+                                    const x_diff = e.pageX - left;
+                                    const per = x_diff / e.currentTarget.clientWidth;
+
+                                    if (per < 0) per = 0;
+                                    if (per > 1) per = 1;
+
+                                    this.ref_audio.current.volume = per;
+
+                                    this.setState({
+                                        volume: per,
+                                    });
+                                }}>
+                                    <div className={classnames(styles.progress, styles.volume_progress)} style={{
+                                        width: `${volume * 100}%`,
+                                    }} />
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </Fragment>
             );
         }
--- src/modules/Posts/Posts.js
+++ src/modules/Posts/Posts.js
@@ -9,6 +9,33 @@ import Post from '../Post';
 import styles from './Posts.module.css';
 
 class Posts extends Component {
+    constructor (props) {
+        super(props);
+
+        this.onScroll = this.onScroll.bind(this);
+    }
+
+    onScroll (e) {
+        const {
+            posts,
+            fetchPosts,
+        } = this.props;
+        const {
+            list = [],
+            subreddit = {},
+        } = posts;
+
+        const c = e.currentTarget;
+        const padding = 0;
+        if (Math.ceil(c.clientHeight + c.scrollTop + padding) >= Math.floor(c.scrollHeight)) {
+            console.log('END');
+            fetchPosts(subreddit, {
+                count: list.length,
+                after: list[list.length - 1].name,
+            });
+        }
+    }
+
     render () {
         const {
             className,
@@ -26,7 +53,7 @@ class Posts extends Component {
         const subreddit_data = subreddits_data[subreddit.name] || {};
 
         return (
-            <div className={classnames(styles.wrapper, className)}>
+            <div className={classnames(styles.wrapper, className)} onScroll={this.onScroll}>
                 {list.map((post) => {
                     if (!post || !post.id) return null;
                     return <Post key={post.id} subreddit={subreddit_data} post={post} />
